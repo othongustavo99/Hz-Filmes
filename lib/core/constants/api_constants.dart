3683829defc3f6243595f5eb589313cfbb8a36ed
Othon <othongustavo99@users.noreply.github.com>
2026-09-09 13:41:47 -0300

    feat: correções

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   
-  static const String apiKey ='3fb520ec1865d9c239702b3be923c20c';
+  static const String apiKey = String.fromEnvironment('TMDB_API_KEY');
   
   static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String imageBaseUrl = 'https://image.tmdb.org/t/p/';
